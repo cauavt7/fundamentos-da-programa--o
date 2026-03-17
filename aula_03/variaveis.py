16bda089cefb8c3c8900594635dc3f9564cd349f
cauã <cuado386@gmail.com>
2026-03-17 09:31:51 -0300

    Subindo os primeiros arquivos

---
+++ aula_03/variaveis.py
@@ -0,0 +1,13 @@
+#variáveis e tipos de dados
+# Em python, você não precisa declarar o tipo de uma linguagem antes de usá-la. o ti é inferido automaticamente pelo interpretador.
+#Em python, a variavel é declaradano padrão snake_case.
+#1. string ->
+#2. nmbeer - int (numero inteiro)
+#3. number - float (número decimal) ->
+#4. boolen - true ou false (verdadeiro ou falso)
+nome_usuario = "caua" # string
+email= "cauado386@gmail.com" # string
+cidade = "Rio de Janeiro" # string
+cpf = 17691242760 # number-int
+salario = 14000.65 # number-float
+casado = False # boolean
---
+++ aula_04/operadores.py
@@ -0,0 +1,18 @@
+# 1. Operadores matemático
+numero_um = 20
+numero_dois = 15
+
+soma = numero_um + numero_dois
+print(f"A soma entre {numero_um} + {numero_dois} = {soma}")
+
+subtracao = numero_um - numero_dois
+print(f"a subtracao entre {numero_um} - {numero_dois} = {subtracao}")
+
+multiplicacao= numero_dois * numero_dois
+print(f"a multiplicaco entre {numero_um} * {numero_dois} = {multiplicacao}")
+
+divisao = numero_dois / numero_dois
+print(f"a divisao entre {numero_um} / {numero_dois} = {divisao}")
+
+porcentagem = numero_um % numero_dois
+print(f"porcentagem entre {numero_um} % {numero_dois} = {porcentagem}")
